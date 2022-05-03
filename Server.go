ad73d43281373737c3b7912fce1a2c0876cb06a3
Nviswateja <41563017+Nviswateja@users.noreply.github.com>
2022-05-03 11:11:49 +0530

    update server

--- Server.go
+++ Server.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net"
+
 	"google.golang.org/grpc"
 
 	pb "github.com/Nviswateja/CustomerTrackerService/service/protos"
@@ -12,7 +15,12 @@ type server struct {
 	pb.UnimplementedCustomerServiceServer
 }
 
-func main(){
+func (c *server) AddCustomerDetails(ctx context.Context, in *pb.CustomerMessageRequest) (*pb.CustomerMessageReply, error) {
+	fmt.Println("Customer details:", in.Name)
+	return &pb.CustomerMessageReply{Message: "Hello " + in.GetName()}, nil
+}
+
+func main() {
 	conn, err := net.Listen("tcp", ":9000")
 	if err != nil {
 		log.Fatalf("Error while connecting to tcp %v", err)
